88a088a5d87214e965f3bfc2f235ba52f94954b8
sole <sole@mozilla.com>
2014-07-03 15:44:30 +0100

    clarify why the interval

--- devicePixelRatio/js/main.js
+++ devicePixelRatio/js/main.js
@@ -3,6 +3,8 @@
 	var p = document.querySelector('p');
 	h1.textContent = document.title;
 
+	// There is no event for when the pixel ratio changes (maybe because you moved the window to a monitor with different density)
+	// So we'll use this interval and listen for changes somewhat often.
 	setInterval(updateDPR, 50);
 
 	function updateDPR() {
